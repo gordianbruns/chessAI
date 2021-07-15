da970d0d19e8a6d91bd625cde0506390808bd76b
gordianbruns <gordob1@web.de>
2021-07-15 15:36:44 -0400

    removed own implementation of chess game

--- chessAI/app.py
+++ chessAI/app.py
@@ -1,14 +1,12 @@
 from chessAI.game.helpers import *
+from chessAI.common.constants import *
+
 import chess
 
 
 def run():
     board = chess.Board()
     play_game(Node(board), "random", "random")
-
-    #board = Board()
-    #board.initialize()
-    #play_game1(Node(board), "random", "random", 0)
 
 
 def play_game(node, function1, function2):
@@ -33,27 +31,6 @@ def play_game(node, function1, function2):
                 node.set_utility(random_strategy())
     next_node = minimax(tree)
     play_game(next_node, function1, function2)
-
-
-def play_game1(node, function1, function2, count):
-    print(node.get_board().get_turn())
-    node.get_board().print()
-    print()
-    if node.get_board().is_terminal():
-        print(node.get_board().get_turn(), "won!")
-        return
-    tree, leaf_nodes = create_state_tree1(node.get_board())
-    if tree.get_board().get_turn() == WHITE:
-        if function1 == "random":
-            for node in leaf_nodes:
-                node.set_utility(random_strategy())
-    else:
-        if function2 == "random":
-            for node in leaf_nodes:
-                node.set_utility(random_strategy())
-    next_node = minimax(tree)
-    count += 1
-    play_game1(next_node, function1, function2, count)
 
 
 if __name__ == '__main__':
--- chessAI/common/constants.py
+++ chessAI/common/constants.py
@@ -1,9 +1,2 @@
-from ..game.figures import *
-
-ROWS = 8
-COLUMNS = 8
 WHITE = "white"
 BLACK = "black"
-
-figDict = {WHITE: {Pawn: "♟", Rook: "♜", Knight: "♞", Bishop: "♝", King: "♚", Queen: "♛"},
-           BLACK: {Pawn: "♙", Rook: "♖", Knight: "♘", Bishop: "♗", King: "♔", Queen: "♕"}}
